db5da6acfead01a1a7a565d33021fe57764818b6
Calatrone <148393551+Calatrone@users.noreply.github.com>
2023-11-25 14:29:09 -0300

    Update Cadastro.js
    teste register "terra e universo"

--- Cadastro.js
+++ Cadastro.js
@@ -1,35 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDNQCgRHw5tRtSelD3LbIef1VUSEuJ3Rwc",
-  authDomain: "ebook-3bc31.firebaseapp.com",
-  databaseURL: "https://ebook-3bc31-default-rtdb.firebaseio.com",
-  projectId: "ebook-3bc31",
-  storageBucket: "ebook-3bc31.appspot.com",
-  messagingSenderId: "988013202215",
-  appId: "1:988013202215:web:8e4f4cc05849594cadf702"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
 function registerUser1() {
 
   user_name = document.getElementById("user_name").value;
-  localStorage.setItem("user_name", user_name);
-  firebase.database().ref("/").child(user_name).update({
-    eixo: "Matéria e Energia"
-  });
   user_mail = document.getElementById("user_mail").value;
+  localStorage.setItem("user_name", user_name);
   localStorage.setItem("user_mail", user_mail);
-  firebase.database().ref("/").child(user_mail).update({
-    mail: "e-mail cadastrado no eixo Matéria e Energia"
+  database().ref("/users/").set({
+    eixo: "Matéria e Energia",
+    name: user_name,
+    mail: user_mail
+    
   });
-  window.location="eixos/terra/terraUniverso.html"
+  
+  window.location.href="https://calatrone.github.io/ebook/eixos/terra/terraUniverso.html"
 }
 
 function registerUser2() {
