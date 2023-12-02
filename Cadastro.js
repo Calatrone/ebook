b5394883d9035085105ef326e46c2291d9de45e6
Calatrone <148393551+Calatrone@users.noreply.github.com>
2023-12-02 10:16:48 -0300

    Update Cadastro.js
    atualização firebase

--- Cadastro.js
+++ Cadastro.js
@@ -1,11 +1,28 @@
 function registerUser1() {
-  console.log("entrando em materia e energia")
+  console.log("entrando em terra e Universo")
   user_name = document.getElementById("user_name").value;
   user_mail = document.getElementById("user_mail").value;
   localStorage.setItem("user_name", user_name);
   localStorage.setItem("user_mail", user_mail);
-  database().ref("/users/").set({
+  firebase.database().ref("/users/").set({
     eixo: "Matéria e Energia",
+    name: user_name,
+    mail: user_mail
+    
+  });
+  
+  window.location.href="https://calatrone.github.io/ebook/eixos/materia/materiaEnergia.html"
+}
+
+function registerUser2() {
+
+  console.log("entrando em terra e Universo")
+  user_name = document.getElementById("user_name").value;
+  user_mail = document.getElementById("user_mail").value;
+  localStorage.setItem("user_name", user_name);
+  localStorage.setItem("user_mail", user_mail);
+  firebase.database().ref("/users/").set({
+    eixo: "Terra e Universo",
     name: user_name,
     mail: user_mail
     
@@ -14,32 +31,20 @@ function registerUser1() {
   window.location.href="https://calatrone.github.io/ebook/eixos/terra/terraUniverso.html"
 }
 
-function registerUser2() {
-
-  user_name = document.getElementById("user_name").value;
-  localStorage.setItem("user_name", user_name);
-  firebase.database().ref("/").child(user_name).update({
-    eixo: "Terra e Universo"
-  });
-  user_mail = document.getElementById("user_mail").value;
-  localStorage.setItem("user_mail", user_mail);
-  firebase.database().ref("/").child(user_mail).update({
-    mail: "e-mail cadastrado no eixo Terra e Universo"
-  });
-  window.location="./eixos/terra/terraUniverso.html"
-}
-
 function registerUser3() {
 
+  console.log("entrando em Vida e Evolução")
   user_name = document.getElementById("user_name").value;
-  localStorage.setItem("user_name", user_name);
-  firebase.database().ref("/").child(user_name).update({
-    eixo: "Vida e Evolução"
-  });
   user_mail = document.getElementById("user_mail").value;
+  localStorage.setItem("user_name", user_name);
   localStorage.setItem("user_mail", user_mail);
-  firebase.database().ref("/").child(user_mail).update({
-    mail: "e-mail cadastrado no eixo Vida e Evolução"
+  firebase.database().ref("/users/").set({
+    eixo: "Vida e Evolução",
+    name: user_name,
+    mail: user_mail
+    
   });
-  window.location="./eixos/vida/vidaEvolucao.html"
+  
+  window.location.href="https://calatrone.github.io/ebook/eixos/vida/vidaEvolucao.html"
+
 }
