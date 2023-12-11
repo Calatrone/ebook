49a10409cea0d54a9d959f4dcd4ab7591b86f8ac
Calatrone <148393551+Calatrone@users.noreply.github.com>
2023-12-11 07:18:40 -0300

    Update Cadastro.js
    firebaseatt

--- Cadastro.js
+++ Cadastro.js
@@ -1,41 +1,31 @@
 function registerUser1() {
-  console.log("entrando em materia e Energia")
   user_name = document.getElementById("user_name").value;
   user_mail = document.getElementById("user_mail").value;
-  localStorage.setItem("user_name", user_name);
-  localStorage.setItem("user_mail", user_mail);
-  firebase.database().ref("/materia/").child(user_name).update({
-    mail: user_mail
-    
-  });
+  firebase.database().ref("/materia-energia/").push({
+    name:user_name,
+    email:user_mail
+   });
   
-  window.location.href="https://calatrone.github.io/ebook/eixos/materia/materiaEnergia.html"
+ window.location.href="https://calatrone.github.io/ebook/eixos/materia/materiaEnergia.html"
 }
 
 function registerUser2() {
-   console.log("entrando em terra e Universo")
   user_name = document.getElementById("user_name").value;
   user_mail = document.getElementById("user_mail").value;
-  localStorage.setItem("user_name", user_name);
-  localStorage.setItem("user_mail", user_mail);
-  firebase.database().ref("/terra/").child(user_name).update({
-    mail: user_mail
-    
-  });
-  
+  firebase.database().ref("/terra-universo/").push({
+    name:user_name,
+    email:user_mail
+   });
   window.location.href="https://calatrone.github.io/ebook/eixos/terra/terraUniverso.html"
 }
 
 function registerUser3() {
-   console.log("entrando em vida e Evolução")
   user_name = document.getElementById("user_name").value;
   user_mail = document.getElementById("user_mail").value;
-  localStorage.setItem("user_name", user_name);
-  localStorage.setItem("user_mail", user_mail);
-  firebase.database().ref("/vida/").child(user_name).update({
-    mail: user_mail
-    
-  });
+  firebase.database().ref("/vida-evolucao/").push({
+    name:user_name,
+    email:user_mail
+   });
   window.location.href="https://calatrone.github.io/ebook/eixos/vida/vidaEvolucao.html"
 
 }
